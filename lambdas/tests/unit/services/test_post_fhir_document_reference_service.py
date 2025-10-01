11644e9398f8f92d4bc5e3f7fbbab14aa9455a60
jameslinnell <jimlinnell@gmail.com>
2025-10-01 11:32:03 +0100

    mtls enums and database lookup

--- lambdas/tests/unit/services/test_post_fhir_document_reference_service.py
+++ lambdas/tests/unit/services/test_post_fhir_document_reference_service.py
@@ -99,6 +99,14 @@ def valid_fhir_doc_json():
 
 
 @pytest.fixture
+def valid_non_mtls_header():
+    return {
+        "Accept": "text/json",
+        "Host": "example.com",
+    }
+
+
+@pytest.fixture
 def valid_mtls_header():
     return {
         "Accept": "text/json",
